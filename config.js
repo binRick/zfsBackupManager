dd9db45fa366da5fd95ac537c0e9441b988248ed
root <root@beo.infinitumtech.net>
2017-08-24 17:34:43 -0400

    added api endpoint

--- config.js
+++ config.js
@@ -4,5 +4,16 @@ module.exports = {
         port: 1488,
         host: '0.0.0.0',
     },
-
+    zfsListMaps: {
+        nodes: function(l) {
+            var ls = l.split('/');
+            return ls[2];
+        },
+    },
+    zfsListFilters: {
+        nodes: function(l) {
+            var ls = l.split('/');
+            return ls.length == 3 && ls[0] == 'tank' && ls[1] == 'Backups';
+        },
+    },
 };
--- webserver.js
+++ webserver.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var app = require('express')(),
+    ZFS = require('./node-zfs'),
     c = require('chalk'),
     _ = require('underscore'),
     config = require('./config'),
@@ -8,8 +9,37 @@ var app = require('express')(),
     io = require('socket.io')(server),
     spawn = require('child_process').spawn;
 
+var zfsList = function(_cb) {
+    var listSpawn = spawn('zfs', ['list', '-pHoname']);
+    var o = '';
+    listSpawn.on('exit', function(code) {
+        if (code != 0) throw code;
+        var lines = o.split('\n').filter(function(l) {
+            return l;
+        }).filter(config.zfsListFilters.nodes).map(config.zfsListMaps.nodes);
+        _cb(lines);
+    }).stdout.on('data', function(s) {
+        o += s.toString();
+    });
+    listSpawn.stderr.on('data', function(s) {
+        throw s;
+    });
+};
+
 app.get('/', function(req, res) {
     res.send('hello');
+});
+app.get('/api/nodes', function(req, res) {
+    zfsList(function(list) {
+        res.json(list);
+    });
+});
+app.get('/api/node/:node', function(req, res) {
+var fs = 'tank/Backups/'+req.params.node;
+    ZFS.zfs.get(fs, ['used','logicalused'], true, function(err, properties) {
+	if(err)throw err;
+	res.json(properties[fs]);
+    });
 });
 
 server.listen(config.webserver.port, config.webserver.host, function() {
